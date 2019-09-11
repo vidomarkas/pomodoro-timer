faff6d14ed930f690ceb6b20fba7a8b5d9a6404d
Viktoras Domarkas <vidomarkas@gmail.com>
2019-09-12 00:24:03 +0100

    settings: disable audio, remove outline

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
                 this.toggleSession();
               }
             }
-          }, 10);
+          }, 1000);
         } else if (this.state.inRest) {
           let duration = this.state.restTime;
 
@@ -63,7 +63,7 @@ class App extends Component {
                 this.toggleSession();
               }
             }
-          }, 10);
+          }, 1000);
         }
       }
     );
